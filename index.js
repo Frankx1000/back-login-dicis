d6b4b24f95b11736a7bf37b0f8cf827e55f96e7c
unknown <f.santacruzhernandezfrancisco>
2023-02-09 22:19:56 -0600

    configuramos el backend

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app = express();
+
+//Capturar el body
+app.use(bodyParser.urlencoded({
+    extended: FALSE 
+}))
+
+app.use(bodyParser.json())
+
+//Conexion a la base de datos 
+const url = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.xy2wfez.mongodb.net/?retryWrites=true&w=majority`
+mongose.connect(url,{
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+}).then(()=> console.log('conectado a BD'))
+.catch((error)=> console.log('Error' + error))
+
+//Creacion e importacion de rutas 
+const authRoutes = require('./routes/auth')
+
+//Middleware
+app.use('/api/user',authRoutes)
+
+app.get('/',(req,res)=>{
+    res.json({
+        estado:true,
+        mensaje : 'Funciona bien'
+    })
+})
+
+//iniciamos el servidor 
+const PORT = process.env.PORT || 10000
+app.listen(PORT,()=>{
+console.log(`Servidor en Puerto : ${PORT} `)
+})
