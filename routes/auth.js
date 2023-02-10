eafb3101db57838e68ea456253778673c6f46b1e
unknown <f.santacruzhernandezfrancisco>
2023-02-10 10:51:03 -0600

    terminamos el crud

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,14 @@ const validacionRegistro = Joi.object({
     password: Joi.string().min(6).max(1024).required()
 })
 
+const validacionUpdate = Joi.object({
+    id: Joi.string().max(1024).required(),
+    name: Joi.string().max(255).required(),
+    apaterno: Joi.string().max(255).required(),
+    amaterno: Joi.string().max(255).required(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
 const validacionLogin= Joi.object({
     email: Joi.string().max(255).required(),
     password: Joi.string().min(6).max(1024).required()
@@ -101,6 +109,89 @@ router.post('/login', async(req, res) => {
         error: null,
         data: { token }
     })
+})
+
+router.get('getallusers', async(req, res) => {
+    const users = await User.find()
+
+    if (users) {
+        res.json({
+            error: null,
+            data: users
+        })
+    } else {
+        res.json({
+            error: "No hay usuarios en la BD"
+        })
+    }
+})
+
+router.post('updateuser', async(req, res) => {
+    const id = req.body.id
+    const userExist = await User.findById({_id: id})
+    if (userExist) {
+        await User.findByIdAndDelete({_id: id})
+        res.json({
+            error: null,
+            data: 'Usuario Borrado'
+        })
+    } else {
+        res.json({
+            error: 'Usuario no encontrado'
+        })   
+    }
+})
+
+router.post('/updateuser', async(req, res) => {
+    const { error } = validacionRegistro.validate(req.body)
+
+    if (error) {
+        return res.status(400).json({
+            error:error.details[0].message
+        })
+    }
+
+    let existeId = await User.findOne({
+        _id: req.body.email
+    })
+
+    if (!existeId) {
+        return res.status(400).json({
+            error: "El usuario no existe"
+        })   
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const contrasenaNueva = await bcrypt.hash(req.body.password,salt)
+    existeId = {
+        id: req.body.id,
+        name: req.body.name,
+        apaterno: req.body.apaterno,
+        amaterno: req.body.amaterno,
+        password: contrasenaNueva
+    }
+
+    try{
+        const guardado = await User.findByIdAndUpdate(
+            { _id: existeId.id },
+            existeId,
+            { new: true}
+        )
+        if(guardado) {
+            return res.json({
+                error:null,
+                data:guardado
+            })
+        } else {
+            return res.json({
+            error:"No se pudo guardar"
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error
+        })
+    }
 })
 
 module.exports = router
